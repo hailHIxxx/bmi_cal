dacabe84c8d8fd78e26e3da06f797c60a344f933
supakorn <kb.supakorn_St@tni.ac.th>
2024-09-24 14:56:28 +0700

    Initial commit

---
+++ bmi_calculator/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace bmi_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.heigh_value = new System.Windows.Forms.TextBox();
+            this.weight_value = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "kg";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "cm";
+            // 
+            // heigh_value
+            // 
+            this.heigh_value.Location = new System.Drawing.Point(97, 86);
+            this.heigh_value.Name = "heigh_value";
+            this.heigh_value.Size = new System.Drawing.Size(100, 20);
+            this.heigh_value.TabIndex = 4;
+            // 
+            // weight_value
+            // 
+            this.weight_value.Location = new System.Drawing.Point(97, 48);
+            this.weight_value.Name = "weight_value";
+            this.weight_value.Size = new System.Drawing.Size(100, 20);
+            this.weight_value.TabIndex = 5;
+            this.weight_value.Text = "5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 209);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.weight_value);
+            this.Controls.Add(this.heigh_value);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox heigh_value;
+        private System.Windows.Forms.TextBox weight_value;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bmi_calculator/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmi_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str_h = heigh_value.Text;
+            string str_w = weight_value.Text;
+           
+            bool null_h = String.IsNullOrEmpty(str_h);
+            bool null_w = String.IsNullOrEmpty(str_w);
+            if (null_h == true || null_w == true)
+            {
+                MessageBox.Show("!! Are you forget something !!", "result");
+                goto stop;
+            }
+                    
+            for (int i = 0; i <= str_h.Length -1; i++)
+            {
+                bool qq = Char.IsLetter(str_h, i);
+                if (qq == true)
+                {
+                    MessageBox.Show("!! Can't put an alphabet here !!", "result");
+                    goto stop;
+                }
+            }
+            for (int j = 0; j <= str_w.Length -1; j++)
+            {
+                bool pp = Char.IsLetter(str_w, j);
+                if (pp == true)
+                {
+                    MessageBox.Show("!! Can't put an alphabet here !!", "result");
+                    goto stop;
+                }
+            }
+
+            double d_h = Convert.ToDouble(str_h);
+            double d_w = Convert.ToDouble(str_w);
+
+            if (d_h <= 0 || d_w <= 0)
+            {
+                MessageBox.Show("!! Your weight or height can't be 0 !!", "result");
+                goto stop;
+            }
+
+            d_h = d_h * 0.01;
+
+            double bmi = d_w / Math.Pow(d_h, 2);
+
+            string ans = "";
+            ans = bmi.ToString("F1");
+
+            if (bmi < 18.5)
+            {
+                MessageBox.Show("Your BMI is " + ans.ToString() + Environment.NewLine + "Under weight", "result");
+            }
+            else if (bmi >= 18.5 && bmi < 25)
+            {
+                MessageBox.Show("Your BMI is " + ans.ToString() + Environment.NewLine + "Normal", "result");
+            }
+            else if (bmi >= 25 && bmi < 30)
+            {
+                MessageBox.Show("Your BMI is " + ans.ToString() + Environment.NewLine + "Over weight", "result");
+            }
+            else if (bmi >= 30 && bmi < 35)
+            {
+                MessageBox.Show("Your BMI is " + ans.ToString() + Environment.NewLine + "Obese", "result");
+            }
+            else
+            {
+                MessageBox.Show("Your BMI is " + ans.ToString() + Environment.NewLine + "Extremely obese", "result");
+            }
+            stop:;
+        }
+    }
+}
